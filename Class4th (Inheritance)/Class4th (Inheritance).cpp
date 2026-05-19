e0a8715d8424833f58348caf808f60e6a8e9f27c
huije08 <huijegang@gmail.com>
2026-05-19 19:39:34 +0900

    (2026.5.19)상속

---
+++ Class4th (Inheritance)/Class4th (Inheritance).cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+using namespace std;
+
+class Consumable
+{
+private:
+
+public:
+	Consumable()
+	{
+		cout << "Cretated Consumable" << endl;
+	}
+	~Consumable()
+	{
+		cout << "Destoryed Consumable" << endl;
+	}
+
+
+};
+
+class Potion : public Consumable
+{
+private:
+	int stamina;
+public:
+	Potion()
+	{
+		cout << "Cretated Potion" << endl;
+	}
+
+	~Potion()
+	{
+		cout << "Destoryed Potion" << endl;
+	}
+};
+int main()
+{
+#pragma region 상속
+	// 상위 클래스의 속성을 하위 클래스가 
+	Consumable consumable;
+	Potion potion;
+
+	cout << "Consumable : " << sizeof(consumable) << endl;
+#pragma endregion
+
+	return 0;
+}
